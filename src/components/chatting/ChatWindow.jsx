6984b781422a866ceafaed6d8f0ba9da2e278220
SaurabhK05 <saurabh.k150600@gmail.com>
2024-03-25 21:51:49 +0530

    formatted ChatWindow component

--- src/components/chatting/ChatWindow.jsx
+++ src/components/chatting/ChatWindow.jsx
@@ -9,6 +9,7 @@ import { roomContext } from "../../App";
 import ReplyCard from "../ChatReplyCard/ReplyCard";
 import UserNav from "../UserSideNav/UserNav";
 import "./styles.css";
+import ChatCard from "../ChatCard/ChatCard";
 
 const ChatWindow = () => {
   const url = process.env.REACT_APP_PROD_API_URL;
@@ -28,6 +29,8 @@ const ChatWindow = () => {
   const [users, setUsers] = useState([]);
   const [chatReply, setChatReply] = useState({});
   const messageInputRef = useRef(null);
+  const chatWindowRef = useRef(null);
+  const chatInnerWindow = useRef(null);
 
   const AddComment = () => {
     if (comment !== "") {
@@ -79,7 +82,19 @@ const ChatWindow = () => {
     setChatReply({});
   };
 
+  // useEffect(() => {
+  //   scrollToBottom();
+  // }, []);
+
+  // Function to scroll to the bottom
+  const scrollToBottom = () => {
+    if (chatWindowRef.current) {
+      chatWindowRef.current.scrollTop = chatWindowRef.current.scrollHeight;
+    }
+  };
+
   useEffect(() => {
+    scrollToBottom();
     if (
       !roomProviderContext.roomDetails.userName ||
       !roomProviderContext.roomDetails.roomCode
@@ -189,90 +204,28 @@ const ChatWindow = () => {
         </div>
         <div className="flex flex-1">
           <UserNav users={users} />
-          <div className="flex-1 min-h-0 p-4 overflow-auto relative">
-            <div className="flex flex-col gap-2">
+          <div
+            className="flex-1 min-h-0 p-4 overflow-auto relative chat-window-container"
+            ref={chatWindowRef}
+          >
+            <div className="flex flex-col gap-2" ref={chatInnerWindow}>
               {chats.map((chat, index) => {
                 if (chat.type === "message") {
                   if (chat.id === userId.current) {
                     return (
-                      <div
-                        key={chat.messageId}
-                        className="flex justify-end gap-2"
-                      >
-                        <div
-                          className="message-container flex flex-col"
-                          id={`chat-box-${index}`}
-                        >
-                          {chat.isRepliedChat ? (
-                            <div className="bg-gray-100 rounded-lg p-4 text-sm break-words dark:bg-gray-800 dark:text-white ">
-                              <ReplyCard
-                                data={chat.repliedMessage}
-                                isReplyCard={false}
-                              />
-                              {chat.message}
-                            </div>
-                          ) : (
-                            <div className="bg-gray-100 rounded-lg p-4 text-sm break-words dark:bg-gray-800 dark:text-white ">
-                              {chat.message}
-                            </div>
-                          )}
-                          <div className="flex items-center gap-2 text-xs text-gray-500 dark:text-gray-400">
-                            <div>{chat.time}</div>
-                            <div
-                              className="chat-reply-container underline cursor-pointer"
-                              onClick={() =>
-                                handleChatReply(
-                                  chat.time,
-                                  chat.message,
-                                  chat.userName,
-                                  chat.date
-                                )
-                              }
-                            >
-                              Reply
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+                      <ChatCard
+                        data={chat}
+                        chatType={"message"}
+                        handleChatReply={handleChatReply}
+                      />
                     );
                   } else {
                     return (
-                      <div
-                        key={"reply" + chat.messageId}
-                        className="flex justify-start gap-2"
-                      >
-                        <div className="message-container flex flex-col">
-                          {chat.isRepliedChat ? (
-                            <div className="bg-gray-100 rounded-lg p-4 text-sm break-words dark:bg-gray-800 dark:text-white ">
-                              <ReplyCard
-                                data={chat.repliedMessage}
-                                isReplyCard={false}
-                              />
-                              {chat.message}
-                            </div>
-                          ) : (
-                            <div className="bg-gray-100 rounded-lg p-4 text-sm break-words dark:bg-gray-800 dark:text-white ">
-                              {chat.message}
-                            </div>
-                          )}
-                          <div className="flex items-center gap-2 text-xs text-gray-500 dark:text-gray-400">
-                            <div>{chat.time}</div>
-                            <div
-                              className="chat-reply-container underline cursor-pointer"
-                              onClick={() =>
-                                handleChatReply(
-                                  chat.time,
-                                  chat.message,
-                                  chat.userName,
-                                  chat.date
-                                )
-                              }
-                            >
-                              Reply
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+                      <ChatCard
+                        data={chat}
+                        chatType={"reply"}
+                        handleChatReply={handleChatReply}
+                      />
                     );
                   }
                 } else if (chat.type === "action" && chat.action === "join")
